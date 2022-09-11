3bc0ae84a328c4bc44963273b3130f2590e0f13a
Vitor Hugo <vitorhugopereiraa@outlook.com>
2022-09-11 16:39:41 -0300

    auth metods finished

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import "../style/main.css";
-function MyApp({ Component, pageProps }) {
-  return <><link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;800&display=swap" rel="stylesheet"></link><Component {...pageProps} /></>
-}
-
-export default MyApp
-//teste
